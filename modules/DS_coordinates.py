e0cfb7195f1d567de9408adeb37e1e50fa99dc91
rt2122 <alisa.nemeshaeva@gmail.com>
2020-07-10 15:23:57 +0500

    automatic

--- modules/DS_coordinates.py
+++ modules/DS_coordinates.py
@@ -4,11 +4,11 @@ TARGET_DEC1 = 54.27
 TARGET_DEC2 = 61.25
 
 def line_in_field(line):
+    import numpy as np
 
     def dot_in_field(ra, dec):
-        if ra <= TARGET_RA2 and ra >= TARGET_RA1 and \
-            dec <= TARGET_DEC2 and dec >= TARGET_DEC1:
-            return True
-        return False
+        return np.logical_and(np.logical_and(np.logical_and(
+            ra <= TARGET_RA2, ra >= TARGET_RA1),
+            dec <= TARGET_DEC2), dec >= TARGET_DEC1)
 
     return dot_in_field(line['RA'], line['DEC'])
