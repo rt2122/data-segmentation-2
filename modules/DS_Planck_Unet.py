f8ba5cad5dc035ed8ffbec575f8236c965aac386
rt2122 <alisa.nemeshaeva@gmail.com>
2020-08-21 18:54:43 +0500

    automatic

--- modules/DS_Planck_Unet.py
+++ modules/DS_Planck_Unet.py
@@ -61,3 +61,167 @@ def draw_pic_with_mask(center, clusters, radius=0.84, size=64, fin_nside=2048,
                               mask.shape, coords_mode=False))
     
     return pic, mask
+
+def nearest_clusters(df, theta, phi, radius=2, galactic=True):
+    from astropy.coordinates import SkyCoord
+    from astropy import units as u
+    import numpy as np
+    sc_cen = None
+    sc_cl = SkyCoord(ra=np.array(df['RA'])*u.degree, 
+                     dec=np.array(df['DEC'])*u.degree, frame='icrs')
+    if galactic:
+        sc_cen = SkyCoord(l=theta*u.degree, b=phi*u.degree, frame='galactic')
+    else:
+        sc_cen = SkyCoord(ra=theta*u.degree, dec=phi*u.degree, frame='icrs')
+    return df[sc_cen.separation(sc_cl).degree < radius]
+
+def pixels_with_clusters(clusters, big_pixels, nside, min_rad=0.62):
+    from astropy.coordinates import SkyCoord
+    from astropy import units as u
+    import pandas as pd
+    import healpy as hp
+    import numpy as np
+    from DS_healpix_fragmentation import radec2pix
+
+    
+    df = pd.read_csv(clusters)
+    pix2 = radec2pix(df['RA'], df['DEC'], 2)
+    df = df[np.in1d(pix2, big_pixels)]
+    df.index = np.arange(df.shape[0])
+    
+    small_pixels = set()
+    sc = SkyCoord(ra=np.array(df['RA'])*u.degree, 
+                  dec=np.array(df['DEC'])*u.degree, frame='icrs')
+    vecs = hp.ang2vec(theta=sc.galactic.l.degree, phi=sc.galactic.b.degree, 
+                      lonlat=True)
+    for i in range(df.shape[0]):
+        small_pixels = small_pixels.union(hp.query_disc(nside, vecs[i], 
+                                                np.radians(min_rad), nest=True))
+    small_pixels = np.array(list(small_pixels))
+    return small_pixels, df
+
+def gen_data(clusters, big_pixels, batch_size, nside=2048, min_rad=0.62, search_nside=512,
+        output=False):
+    import healpy as hp
+    import numpy as np
+    from DS_healpix_fragmentation import radec2pix
+    from DS_Planck_Unet import draw_pic_with_mask
+    #from tqdm.notebook import tqdm
+    from astropy.coordinates import SkyCoord
+    from astropy import units as u
+    
+    small_pixels, df = pixels_with_clusters(clusters, big_pixels, search_nside, min_rad)
+    
+    while True:
+        ipix = np.random.choice(small_pixels, batch_size)
+        theta, phi = hp.pix2ang(nside=search_nside, nest=True, ipix=ipix, lonlat=True)
+        sc = SkyCoord(l=theta*u.degree, b=phi*u.degree, frame='galactic')
+        ra = sc.icrs.ra.degree
+        dec = sc.icrs.dec.degree
+        pics = []
+        masks = []
+        i = 0
+        while i < batch_size:
+            cl_list = nearest_clusters(df, theta[i], phi[i], galactic=True)
+            cl_list = np.stack([cl_list['RA'], cl_list['DEC']]).T
+            pic, mask = draw_pic_with_mask([ra[i], dec[i]], cl_list)
+            if output:
+                print(i, ipix[i], pic.shape, ra[i], dec[i])
+            if not(pic.shape[0] == 64 and pic.shape[1] == 64):
+                small_pixels = small_pixels[small_pixels != ipix[i]]
+                ipix[i] = np.random.choice(small_pixels)
+                theta[i], phi[i] = hp.pix2ang(nside=search_nside, nest=True, 
+                        ipix=ipix[i], lonlat=True)
+                sc_cur = SkyCoord(l=theta[i]*u.degree, b=phi[i]*u.degree, frame='galactic')
+                ra[i] = sc_cur.icrs.ra.degree
+                dec[i] = sc_cur.icrs.dec.degree
+            else:
+                pics.append(pic)
+                masks.append(mask)
+                i += 1
+        yield np.stack(pics), np.stack(masks)
+
+
+def iou(y_pred, y_true):
+    from tensorflow.keras import backend as K
+    iou_sum = 0
+    for i in range(y_true.shape[-1]):
+        inters = K.sum(y_pred[..., i] * y_true[..., i])
+        union = K.sum((y_pred[..., i] + y_true[..., i])) - inters
+        iou_sum += inters / union
+    return iou_sum
+
+def dice(y_pred, y_true, eps=0.1):
+    from tensorflow.keras import backend as K
+    dice_sum = 0
+    for i in range(y_true.shape[-1]):
+        inters = K.sum(y_pred[..., i] * y_true[..., i])
+        union = K.sum((y_pred[..., i] + y_true[..., i])) - inters
+        dice_sum += K.mean((2 * inters + eps) / (union + eps))
+    return dice_sum
+
+
+def unet_planck(input_size = (64,64,6), filters=16, blocks=5, output_layers=1, weights=None): 
+    import numpy as np 
+    from tensorflow.keras.callbacks import ModelCheckpoint, LearningRateScheduler
+    from tensorflow.keras.optimizers import Adam, SGD
+    from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dropout, concatenate, UpSampling2D
+    from tensorflow.keras import Input
+    from tensorflow.keras.callbacks import ModelCheckpoint
+    from tensorflow.keras.models import Model
+    from tensorflow.keras.layers import Activation
+    from tensorflow.keras.activations import relu, sigmoid
+    from tensorflow.keras.layers import UpSampling2D
+    from tensorflow.keras.losses import binary_crossentropy, categorical_crossentropy,\
+            sparse_categorical_crossentropy
+    from tensorflow.keras.layers import BatchNormalization, Dropout
+    from tensorflow.keras import backend as K
+    from tensorflow.keras.models import load_model
+
+    encoder = []
+    inputs = Input(input_size)
+    prev = inputs
+    for i in range(blocks):
+        cur = Conv2D(filters=filters, kernel_size=(3, 3), padding = 'same', kernel_initializer = 'he_normal')(prev)
+        #cur = BatchNormalization()(cur)
+        cur = Activation(relu)(cur)
+        cur = Dropout(0.2)(cur)
+
+        cur = Conv2D(filters=filters, kernel_size=(3, 3), padding = 'same', kernel_initializer = 'he_normal')(cur)
+        #cur = BatchNormalization()(cur)
+        cur = Dropout(0.2)(cur)
+        cur = Activation(relu)(cur)
+
+        encoder.append(cur)
+
+        cur = MaxPooling2D(padding='valid')(cur)
+
+        filters *= 2
+        prev = cur
+    for i in range(blocks - 1, -1, -1):
+        cur = UpSampling2D()(prev)
+        cur = Conv2D(filters=filters, kernel_size=3, padding='same')(cur)
+        #cur = Dropout(0.2)(cur)
+        cur = Activation(relu)(cur)
+        cur = concatenate([cur, encoder[i]], axis=3)
+
+        cur = Conv2D(filters=filters, kernel_size=3, padding='same')(cur)
+        cur = Activation(relu)(cur)
+        #cur = Dropout(0.2)(cur)
+        #cur = Conv2D(filters=filters, kernel_size=3, padding='same')(cur)
+        #cur = Activation(relu)(cur)
+
+        prev = cur
+        filters //= 2
+
+    if not (weights is None):
+        pt = Model(inputs=inputs, outputs=prev)
+        pt.load_weights(weights)
+
+    prev = Conv2D(output_layers, kernel_size=3, padding='same')(prev)
+    prev = Activation(sigmoid)(prev)
+    
+    model = Model(inputs=inputs, outputs=prev)
+    model.compile(optimizer = Adam(lr = 1e-4), loss = binary_crossentropy, metrics = ['accuracy', iou, dice])
+    
+    return model
