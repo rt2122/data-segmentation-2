dc729326d7d3834ef0062d2aa088e83dd8449897
rt2122 <alisa.nemeshaeva@gmail.com>
2020-09-29 08:38:50 +0300

    automatic

---
+++ text/dp_ref/build.sh
@@ -0,0 +1,31 @@
+
+# Pdflatex
+PDFLATEX='pdflatex -interaction=nonstopmode -shell-escape -file-line-error'
+GREP='grep ".*:[0-9]*:.*"' # показывает на выходе только ошибки
+
+# Файлы/Папки
+PDF_NAME='thesis.pdf'
+TEX='tex'
+IMG='tex/inc/img'
+MAINTEX='0-main'
+
+# Конвертация eps
+find $IMG/ -type f -name "*.eps" -exec epstopdf {} ";" ;
+find $IMG -type f -name "*.eps" -exec rm -f {} \;
+
+# Сборка latex
+cd tex
+$PDFLATEX $MAINTEX > /dev/null
+BIBOUTPUT=$(bibtex $MAINTEX)
+# Показывать output bibtex'a только в случае ошибок
+if [[ "$BIBOUTPUT" =~ "error" ]]; then
+    echo "$BIBOUTPUT"
+fi
+$PDFLATEX $MAINTEX > /dev/null
+$PDFLATEX $MAINTEX
+
+cp $MAINTEX.pdf ../$PDF_NAME
+cd ..
+
+
+
